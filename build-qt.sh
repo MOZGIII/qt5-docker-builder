878f0433fb1fde42f14cfd32d8e2db63122a736b
MOZGIII <mike-n@narod.ru>
2016-06-24 01:32:17 +0300

    Added the ability to pass --preserve option at build-qt.sh

--- build-qt.sh
+++ build-qt.sh
@@ -5,10 +5,18 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Read positional arguments
 BUILDER_IMAGE_NAME="$1"
 GIT_BRANCH="$2"
+shift; shift
+
+# Read flags
+while [[ $# -gt 0 ]]; do
+  [[ "$1" == '--preserve-source' ]] && PRESERVE_SOURCE_DIR="yes"
+  [[ "$1" == '--no-preserve-source' ]] && PRESERVE_SOURCE_DIR="no"
+  shift
+done
 
 # Ensure mandatory arguments are set
 if [[ -z "$BUILDER_IMAGE_NAME" || -z "$GIT_BRANCH" ]]; then
-  echo "Usage: $0 <builder image name> <qt git branch>" >&2
+  echo "Usage: $0 <builder image name> <qt git branch> [--preserve-source | --no-preserve-source]" >&2
   exit 1
 fi
 
@@ -21,9 +29,12 @@ run_build_container() {
   docker run -it --rm --name "qt5-builder" -v "$SCRIPTS_DIR:/build-scripts" -v "$HOST_SOURCE_DIR:$DOCKER_SOURCE_DIR" $@
 }
 
+# Build optional args
+[[ "$PRESERVE_SOURCE_DIR" == "yes" ]] && PREPARE_SOURCE_FLAGS="$PREPARE_SOURCE_FLAGS --preserve"
+
 # Trace intersting stuff
 set -x
 
 mkdir -p $HOST_SOURCE_DIR
-run_build_container "$BUILDER_IMAGE_NAME" /bin/bash "/build-scripts/prepare-sources.sh $DOCKER_SOURCE_DIR" "$GIT_BRANCH"
+run_build_container "$BUILDER_IMAGE_NAME" /bin/bash "/build-scripts/prepare-sources.sh $DOCKER_SOURCE_DIR $GIT_BRANCH $PREPARE_SOURCE_FLAGS"
 run_build_container "$BUILDER_IMAGE_NAME" /bin/bash "/build-scripts/build.sh $DOCKER_SOURCE_DIR $DOCKER_SOURCE_DIR"
